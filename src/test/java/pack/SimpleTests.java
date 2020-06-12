62c93b15f6e14d743200e6ef365bdeca694a9d16
kris <k.verenitch@yandex.ru>
2020-06-12 15:21:28 +0400

    commit_2

--- src/test/java/pack/SimpleTests.java
+++ src/test/java/pack/SimpleTests.java
@@ -1,19 +1,81 @@
 package pack;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 public class SimpleTests {
-    @Test
-    public void FirstTest()
+    @DataProvider(name = "FirstModuleData")
+    public Object[][] createData1()
     {
-        System.out.println("Первый тест");
+        double max = Integer.MAX_VALUE - 1;
+        double min = Integer.MIN_VALUE + 1;
+        int n = (int)(Math.random() * max);
+        int m = (int)(Math.random() * min);
+        int s = n + m;
+        return new Object[][]
+                {
+                        {s}
+                };
     }
-    @Test
-    public void SecondTest()
+    @DataProvider(name = "SecondModule1Data")
+    public Object[][] createData2()
     {
-        System.out.println("Второй тест");
+        double max = Integer.MAX_VALUE - 1;
+        double min = Integer.MIN_VALUE + 1;
+        int n = (int)(Math.random() * 1000);
+        int m = (int)(Math.random() * 200);
+        return new Object[][]
+                {
+                        {n, m}
+                };
     }
-    @Test
-    public void ThirdTest()
+
+    @DataProvider(name = "ThirdModule1Data")
+    public Object[][] createData5()
     {
-        System.out.println("Третий тест");
+        double max = Integer.MAX_VALUE - 1;
+        int n = 100;
+        int m = 5;
+        int result = n/m;
+        return new Object[][]
+                {
+                        {n, m, result}
+                };
+    }
+    @Test(dataProvider = "FirstModuleData")
+    public void FirstModule(int n)
+    {
+        System.out.println("Тест 1. Проверка метода Math.abs(n): ");
+
+        int m = Math.abs(n);
+        System.out.println("n = " + n);
+        System.out.println("Модуль n = " + m);
+        if (n < 0) Assert.assertEquals(m,n * -1);
+        else Assert.assertEquals(m, n);
+        System.out.println();
+    }
+    @Test(dataProvider = "SecondModule1Data")
+    public void SecondModule1(int n, int m)
+    {
+        System.out.println("Тест 2. Проверка метода Math.addExact: ");
+        System.out.println("n = " + n);
+        System.out.println("m = " + m);
+        int sum = Math.addExact(n,m);
+        Assert.assertEquals(sum,n + m);
+        System.out.println("Значение с помощью addExact = " + sum);
+        System.out.println("Проверочное значение = " + (n + m));
+        System.out.println();
+    }
+
+    @Test(dataProvider = "ThirdModule1Data")
+    public void ThirdModule1(int n, int m, int res)
+    {         //Происходит деление n на m и остаток округляется
+        System.out.println("Тест 3. Проверка функции Math.floorDiv: ");
+        int result = Math.floorDiv(n, m);
+        System.out.println("Первое число = " + n + "; Второе число = "
+                + m);
+        System.out.println("Результат floorDiv: " + result);
+        System.out.println("Проверка: " + res);
+        Assert.assertEquals(result, res);
+        System.out.println();
     }
 }
