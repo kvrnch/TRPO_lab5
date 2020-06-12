ac3044b871452f8c603c2fc14e4691310d8681a0
kris <k.verenitch@yandex.ru>
2020-06-12 14:29:25 +0400

    Initial commit

---
+++ src/test/java/pack/SimpleTests.java
@@ -0,0 +1,19 @@
+package pack;
+import org.testng.annotations.Test;
+public class SimpleTests {
+    @Test
+    public void FirstTest()
+    {
+        System.out.println("Первый тест");
+    }
+    @Test
+    public void SecondTest()
+    {
+        System.out.println("Второй тест");
+    }
+    @Test
+    public void ThirdTest()
+    {
+        System.out.println("Третий тест");
+    }
+}
